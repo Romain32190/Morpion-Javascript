993882d796620af2267766fd3bbc09381ec1e29b
Romain32190 <romain.lafargue36@gmail.com>
2017-04-13 12:14:21 +0200

    Maj

--- app.js
+++ app.js
@@ -1,26 +1,71 @@
-  var count = 0;
-  var player;
+var count = 0;
+var player = "";
+var tableau = [[1, 2, 3], 
+				[4, 5, 6],
+			 	[7, 8, 9]];
 
-  $("td").click(function()
-  {
 
-    var triche=$(this).html();
+$("td").click(function() {
+	
+	var icone = $(this).attr('style');
+		
+		if (icone === undefined) {
 
-    if(triche){
-    return;
-    }
-    
-    count++
-    if (count % 2 === 0)
-    {
-        $(this).html("<center><img src=https://iut-info.univ-reims.fr/users/cutrona/intranet/progwebS2-old/morpion/rond.png></center>"); 
-    }
-    else
-    {
-        $(this).html("<center><img src=http://www.icône.com/images/icones/2/6/checkmark.png></center>");
-    }
+			count++;		
+			player =   (count % 2 === 0) ? 'player2' : 'player1';
 
+			var nmCol = $(this).index();
+			var nmRow = $(this).parent().index();
+
+			if (player === "player1") { 
+				$(this).css("background","white url(croix.png) no-repeat center");
+				tableau[nmCol][nmRow] = "player1" ;
+			
+			}else{	
+				$(this).css("background","white url(rond.png) no-repeat center");
+				tableau[nmCol][nmRow] = "player2" ;
+			}       
+			
+		}
+		
+		if (tableau[0][0]===tableau[1][0] && tableau[1][0]===tableau[2][0] ){
+			alert("vous avez gagné");
+		}
+
+		if (tableau[0][1]===tableau[1][1] && tableau[1][1]===tableau[2][1] ){
+			alert("vous avez gagné");
+		}
+
+		if (tableau[0][2]===tableau[1][2] && tableau[1][2]===tableau[2][2] ){
+			alert("vous avez gagné");
+		}
+
+		if (tableau[0][0]===tableau[0][1] && tableau[0][1]===tableau[0][2] ){
+			alert("vous avez gagné");
+		}
+
+		if (tableau[1][0]===tableau[1][1] && tableau[1][1]===tableau[1][2] ){
+			alert("vous avez gagné");
+		}
+
+		if (tableau[2][0]===tableau[2][1] && tableau[2][1]===tableau[2][2] ){
+			alert("vous avez gagné");
+		}
+
+		if (tableau[0][0]===tableau[1][1] && tableau[1][1]===tableau[2][2] ){
+			alert("vous avez gagné");
+		}
+
+		if (tableau[2][0]===tableau[1][1] && tableau[1][1]===tableau[0][2] ){
+			alert("vous avez gagné");
+		}
 });
+
+
+
+
+
+
 
 
 
